43581c5d60fc7d961ef9a93ec736e48a28447023
jjresher <jjresher@gmail.com>
2025-02-24 22:53:53 -0500

    melo

---
+++ resources/views/home/index.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('title', 'Home Page - Online Store')
+
+@section('content')
+    <div class="text-center">
+        <h2>Welcome to the application</h2>
+    </div>
+@endsection
